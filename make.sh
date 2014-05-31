7abc7f3cb610cbc6db1c2d2a659e80bbb0826f2c
Daan van Es <3D-ES@users.noreply.github.com>
2014-05-31 22:07:53 +0200

    Multiple new features added
    - Only compiles changed files for faster recompilation.
    - Shows less output to keep the compilation result clean.
    - Added command line parameter 'clean' to clean the project.
    - Added command line parameter 'verbose' to show more output.
    - Removed the (not so interesting) ELF file statistics table.
    - First implementation of MacOS support, and it almost works.

--- make.sh
+++ make.sh
@@ -7,55 +7,57 @@
 # - Downloads the selected firmware from github.com
 # - Compiles the RepRapFirmware source code
 #
-# Hope this works for you too :D
+# Update 30/05/2014:
+#
+# - It only compiles changed files for faster recompilation.
+# - It shows less output to keep the compilation result clean.
+# - Added command line parameter 'clean' to clean project.
+# - Added command line parameter 'verbose' to show output.
+# - Removed the (not so interesting) ELF file statistics.
+# - Probably supports the MacOS platform for jstck...
 #
 # 3D-ES
 #----------------------------------------------------------------------------
-#
+
 # Exit immediately if a command exits with a non-zero status.
-#
 set -e
-#
+
 # Firmware name
-#
 FW=RepRapFirmware
 LIB=${FW}/Libraries
 BUILD=${FW}/Build
 RELEASE=${FW}/Release
-#
+
 # Arduino version to use
-#
-ARDUINO=./arduino-1.5.4
+ARDUINO=arduino-1.5.4
 ARDUINO_VERSION=154
+
+# Check if there are script parameters:
 #
-# Is the Arduino folder valid?
+# - Supports 'clean' to remove the built files.
+# - Supports 'verbose' to display script output.
 #
-if [ ! -d ${ARDUINO} ]; then
-    #
-    # No? Then we need to get it!
-    # Do we need 32 or 64 bits?
-    if [ $(uname -m) == x86_64 ]
-        then ARCHIVE=${ARDUINO}-linux64.tgz
-        else ARCHIVE=${ARDUINO}-linux32.tgz
+if [ $# -gt 0 ]
+then
+    if [ $1 == "clean" ]
+    then
+        echo Cleaning ${BUILD} and ${RELEASE}
+        rm -rf ${BUILD} ${RELEASE}
+        exit
     fi
-    #
-    # Do we have that file cached?
-    if [ ! -f ${ARCHIVE} ]; then
-        #
-        # No? Then download a copy from arduino.cc
-        wget http://downloads.arduino.cc/${ARCHIVE}
+
+    if [ $1 == "verbose" ]
+    then
+        set -x
     fi
-    #
-    # Extract the file.
-    tar -xvzf${ARCHIVE}
 fi
+
+# Check if the RepRapFirmware folder is available:
 #
-# Is there a firmware?
+# - Ask which firmware version to download.
+# - Get the duet branch from the repository.
 #
 if [ ! -d ${FW} ]; then
-    #
-    # No? Ask the user what version to download.
-    #
     while true; do
         echo "Which firmware version do you want to use?"
         echo ""
@@ -69,47 +71,125 @@ if [ ! -d ${FW} ]; then
             * ) echo "Please answer R or D.";;
         esac
     done
-    #
-    # And download it from the repository.
-    #
+
     git clone -b duet ${FW_REPO} ${FW}
 fi
+
+# Check if the Arduino folder is available:
 #
-# Apply DC42's Arduino core patches.
+# - Detect if the hardware is 32 or 64 bits.
+# - Detect if the system runs Linux or MacOS.
+# - Download the archive if no cache found.
+# - Extract the archive to this folder.
 #
-if [ -d ${FW}/ArduinoCorePatches ]; then
-    #
-    # Always overwrite patched files because of git pull updates.
-    #
-    cp -r ${FW}/ArduinoCorePatches/sam ${ARDUINO}/hardware/arduino
+if [ ! -d ${ARDUINO} ]
+then
+    if [[ $OSTYPE == linux-gnu ]]
+    then
+        if [ $(uname -m) == x86_64 ]
+            then ARCHIVE=${ARDUINO}-linux64.tgz
+            else ARCHIVE=${ARDUINO}-linux32.tgz
+        fi
+
+        if [ ! -f ${ARCHIVE} ]
+            then wget http://downloads.arduino.cc/${ARCHIVE}
+        fi
+
+        tar -xvzf ${ARCHIVE}
+    fi
+
+    if [[ $OSTYPE == darwin* ]]
+    then
+        ARCHIVE=${ARDUINO}-macosx.zip
+
+        if [ ! -f ${ARCHIVE} ]
+            then wget http://downloads.arduino.cc/${ARCHIVE}
+        fi
+
+        unzip -o ${ARCHIVE}
+
+        # Convert the Arduino environment to look like Linux.
+	mv Arduino.app/Contents/Resources/Java/* Arduino.app
+	mv Arduino.app ${ARDUINO}
+    fi
 fi
+
+# Check if there are ArduinoCorePatches available:
 #
-# Do we have the library?
+# - These patches are only available with dc42's firmware.
+# - Always overwrites patched files because of git pull updates.
+# - Preserves the timestamps to prevent it's recompilation.
 #
-if [ ! -d ${LIB} ]; then
-    #
-    # The RRP version does not include jmgiacalone's library.
-    #
+if [ -d ${FW}/ArduinoCorePatches ]
+then
+    cp -r --preserve=timestamps ${FW}/ArduinoCorePatches/sam ${ARDUINO}/hardware/arduino
+fi
+
+# Check if jmgiacalone's libraries are available:
+#
+# - The dc42 firmware version contains updated libraries.
+# - The RepRapPro version does not include these libraries.
+#
+if [ ! -d ${LIB} ]
+then
     git clone git://github.com/jmgiacalone/Arduino-libraries ${LIB}
 fi
-#
-# Platform defines that I have copied from the Eclipse output window...
-#
-PLATFORM="-mcpu=cortex-m3 -DF_CPU=84000000L -DARDUINO=${ARDUINO_VERSION} -D__SAM3X8E__ -mthumb"
-#
-# USB defines that I have copied from the Eclipse output window...
-#
-USB_OPT="-DUSB_PID=0x003e -DUSB_VID=0x2341 -DUSBCON"
-#
-# Compiler options that I have copied from the Eclipse output window...
-#
-GCC_OPT="-c -g -O3 -w -ffunction-sections -fdata-sections -nostdlib --param max-inline-insns-single=500 -Dprintf=iprintf -MMD -MP"
-GPP_OPT="-c -g -O3 -w -ffunction-sections -fdata-sections -nostdlib --param max-inline-insns-single=500 -Dprintf=iprintf -MMD -MP -fno-rtti -fno-exceptions -x c++"
+
+# Platform defines
+PLATFORM=(
+    -mcpu=cortex-m3
+    -DF_CPU=84000000L
+    -DARDUINO=${ARDUINO_VERSION}
+    -D__SAM3X8E__
+    -mthumb
+)
+
+# USB defines
+USB_OPT=(
+    -DUSB_PID=0x003e
+    -DUSB_VID=0x2341
+    -DUSBCON
+)
+
+# See: http://gcc.gnu.org/onlinedocs/gcc/Option-Summary.html
+GCC_OPT=(
+    -c # Compile the source files, but do not link
+    -g # Produce debugging info in OS native format
+    -O3 # Maximum optimization of size vs execution time
+    -w # Inhibit all warning messages
+    -ffunction-sections # Place each function into its own section
+    -fdata-sections # Place each data item into its own section
+    -nostdlib # Do not use standard system startup files or libraries
+    --param max-inline-insns-single=500 # Max. instructions to inline
+    -Dprintf=iprintf # Prevent bloat by using integer printf function
+    -MMD # Create dependency output file but only of user header files
+    -MP # Add phony target for each dependency other than the main file
+)
+
+# See: http://gcc.gnu.org/onlinedocs/gcc/Option-Summary.html
+GPP_OPT=(
+    -c # Compile the source files, but do not link
+    -g # Produce debugging info in OS native format
+    -O3 # Maximum optimization of size vs execution time
+    -w # Inhibit all warning messages
+    -ffunction-sections # Place each function into its own section
+    -fdata-sections # Place each data item into its own section
+    -nostdlib # Do not use standard system startup files or libraries
+    --param max-inline-insns-single=500 # Max. instructions to inline
+    -Dprintf=iprintf # Prevent bloat by using integer printf function
+    -MMD # Create dependency output file but only of user header files
+    -MP # Add phony target for each dependency other than the main file
+    -fno-rtti # Don't produce Run-Time Type Information structures
+    -fno-exceptions # Disable exception handling to prevent overhead
+    -x c++ # Specify the language of the input files
+)
+
+# See: http://gcc.gnu.org/onlinedocs/gcc/Option-Summary.html
 COM_OPT=(
     -Os
     -mcpu=cortex-m3
     "-T${ARDUINO}/hardware/arduino/sam/variants/arduino_due_x/linker_scripts/gcc/flash.ld"
-    "-Wl,-Map,${BUILD}/RepRapFirmware.map"
+    "-Wl,-Map,${BUILD}/${FW}.map"
     "-L${BUILD}"
     -lm
     -lgcc
@@ -127,32 +207,8 @@ COM_OPT=(
     ${ARDUINO}/hardware/arduino/sam/variants/arduino_due_x/libsam_sam3x8e_gcc_rel.a
     -Wl,--end-group
 )
-#
-# Location of the ARM compiler.
-#
-GCC_ARM_DIR=${ARDUINO}/hardware/tools/g++_arm_none_eabi
-#
-# Cross compiler binary locations.
-#
-AR=${GCC_ARM_DIR}/bin/arm-none-eabi-ar
-GCC=${GCC_ARM_DIR}/bin/arm-none-eabi-gcc
-GPP=${GCC_ARM_DIR}/bin/arm-none-eabi-g++
-SIZE=${GCC_ARM_DIR}/bin/arm-none-eabi-size
-COPY=${GCC_ARM_DIR}/bin/arm-none-eabi-objcopy
-#
-# Show commands and their arguments as they are executed.
-#
-set -x
-#
-# Make clean.
-#
-rm -rf ${BUILD}
-rm -rf ${RELEASE}
-mkdir -p ${BUILD}
-mkdir -p ${RELEASE}
-#
+
 # Folders to include.
-#
 INC=(
     -I"${FW}/Flash"
     -I"${FW}/Libraries/EMAC"
@@ -170,36 +226,58 @@ INC=(
     -I"${ARDUINO}/hardware/arduino/sam/system/CMSIS/CMSIS/Include/"
     -I"${ARDUINO}/hardware/arduino/sam/libraries/Wire"
 )
-#
-# Find all the .c files and compile them.
-#
-for file in $(find ${FW} ${ARDUINO}/hardware/arduino/sam/cores -type f -name "*.c"); do
+
+# Location of the ARM compiler.
+GCC_ARM_DIR=${ARDUINO}/hardware/tools/g++_arm_none_eabi
+
+# Cross compiler binary locations.
+GCC=${GCC_ARM_DIR}/bin/arm-none-eabi-gcc
+GPP=${GCC_ARM_DIR}/bin/arm-none-eabi-g++
+COPY=${GCC_ARM_DIR}/bin/arm-none-eabi-objcopy
+
+# Create output folders.
+mkdir -p ${BUILD} ${RELEASE}
+
+# Locate and compile all the .c files that need to be compiled.
+for file in $(find ${FW} ${ARDUINO}/hardware/arduino/sam/cores -type f -name "*.c")
+do
+    # Intermediate build output.
     D=${BUILD}/$(basename $file).d
     O=${BUILD}/$(basename $file).o
-    ${GCC} ${GCC_OPT} ${PLATFORM} ${USB_OPT} ${INC[@]} ${file} -MF${D} -MT${D} -o${O}
+
+    # Skip compile if the object is up-to-date.
+    if [ ${O} -nt ${file} ]; then continue; fi
+
+    # Show some progress.
+    echo Compiling ${file}
+
+    # The C source file is newer than the object output file, we need to compile it.
+    ${GCC} ${GCC_OPT[@]} ${PLATFORM[@]} ${USB_OPT[@]} ${INC[@]} ${file} -MF${D} -MT${D} -o${O}
 done
-#
-# Find all the .cpp files and compile them.
-#
-for file in $(find ${FW} ${ARDUINO}/hardware/arduino/sam -type f -name "*.cpp"); do
+
+# Locate and compile all the .cpp files that need to be compiled.
+for file in $(find ${FW} ${ARDUINO}/hardware/arduino/sam -type f -name "*.cpp")
+do
+    # Intermediate build output.
     D=${BUILD}/$(basename $file).d
     O=${BUILD}/$(basename $file).o
-    ${GPP} ${GPP_OPT} ${PLATFORM} ${USB_OPT} ${INC[@]} ${file} -MF${D} -MT${D} -o${O}
+
+    # Skip compile if the object is up-to-date.
+    if [ ${O} -nt ${file} ]; then continue; fi
+
+    # Show some progress.
+    echo Compiling ${file}
+
+    # The CPP source file is newer than the object output file, we need to compile it.
+    ${GPP} ${GPP_OPT[@]} ${PLATFORM[@]} ${USB_OPT[@]} ${INC[@]} ${file} -MF${D} -MT${D} -o${O}
 done
-#
+
 # Combine the objects into an executable file.
-#
-${GPP} ${COM_OPT[@]} -o ${BUILD}/RepRapFirmware.elf
-#
+${GPP} ${COM_OPT[@]} -o ${BUILD}/${FW}.elf
+
 # Convert the ELF file to the firmware binary.
-#
-${COPY} -O binary ${BUILD}/RepRapFirmware.elf ${RELEASE}/RepRapFirmware.bin
-#
-# Output the details of the ELF binary.
-#
-${SIZE} -A ${BUILD}/RepRapFirmware.elf
-#
+${COPY} -O binary ${BUILD}/${FW}.elf ${RELEASE}/${FW}.bin
+
 # Show the output file name.
-#
-echo Created ${RELEASE}/RepRapFirmware.bin
+echo Created ${RELEASE}/${FW}.bin
 
